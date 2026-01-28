bd47b5f73328fce03c716db0948a106250ffcc34
Jarren San Jose <sanjose.jarren@gmail.com>
2026-01-28 11:31:30 -0800

    chore: bump version to 1.0.1 for App Store resubmission

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'Color Cape',
     slug: 'my-app',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'myapp',
